de45a2e8bfd3fcd415bcb6fb4bde5523514e0f39
Hartaithan <hartaithan@gmail.com>
2023-09-01 21:03:09 +0900

    setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'breeween-a9a0a',
+    appId: '1:176573941805:web:eae9ea619a714b641310a4',
+    storageBucket: 'breeween-a9a0a.appspot.com',
+    apiKey: 'AIzaSyBmoinY6L9eJzEZK2xnz8QJ5lOGCR8x3t4',
+    authDomain: 'breeween-a9a0a.firebaseapp.com',
+    messagingSenderId: '176573941805',
+  },
+};
